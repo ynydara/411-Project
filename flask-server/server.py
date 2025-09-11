b5ad9f12f0604c72bfa5b6b5cce23dd8c2e700f5
Alyssa Major <ynydara@gmail.com>
2025-09-11 13:54:34 -0500

    added database. this deletes everything in the databased and inserts values into the leaderboard. this is very janky, press the link and go to url and then press enter and you will get json values, not in front end just yet but we can see the stuff

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,4 +1,48 @@
-from flask import Flask
+from flask import Flask, jsonify
+import psycopg2
+
+#run this only once pls
+def initdb():
+    conn = psycopg2.connect(
+        host="postgres",
+        database="mydb",
+        user="user",
+        password="password"
+    )
+    cur = conn.cursor()
+    cur.execute("""
+                CREATE TABLE IF NOT EXISTS leaderboard
+                (
+                    id SERIAL PRIMARY KEY,
+                    name TEXT NOT NULL,
+                    score INT NOT NULL
+                );
+                """)
+
+    # Insert some default data
+    cur.execute("DELETE FROM leaderboard;")
+    cur.execute("SELECT COUNT(*) FROM leaderboard;")
+    if cur.fetchone()[0] == 0:  # only insert if empty
+        cur.execute("""
+                    INSERT INTO leaderboard (name, score)
+                    VALUES ('Amber', 100),
+                           ('Julie', 80),
+                           ('Alyssa', 60);
+                    """)
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+#this is the connection for da database
+def getdbconnection():
+    conn = psycopg2.connect(
+        host="postgres",
+        database="mydb",
+        user="user",
+        password="password"
+    )
+    return conn
 
 app = Flask(__name__)
 
@@ -7,9 +51,17 @@ app = Flask(__name__)
 def dashboard():
     return "Dashboard"
 
-@app.route('/api/leaderboard')
-def leaderboard():
-    return {"leaderboard": ["member1 on leaderboard", "member2 on leaderboard"]}
+
+@app.route('/api/leaderboard', methods=['GET'])
+def getleaderboard():
+    conn = getdbconnection()
+    cur = conn.cursor()
+    cur.execute('SELECT name, score FROM leaderboard ORDER BY score DESC;')
+    rows = cur.fetchall()
+    cur.close()
+    conn.close()
+    data = [{"name": r[0], "score": r[1]} for r in rows]
+    return jsonify(data)
 
 @app.route('/api/insights')
 def insights():
@@ -25,4 +77,5 @@ def settings():
 
 
 if __name__ == '__main__':
+    initdb()
     app.run(host="0.0.0.0", port=5000, debug=True)
